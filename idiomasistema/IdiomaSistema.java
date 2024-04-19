46d27d5f7c26704d364a45fa9395abaf6f41f20a
andersongulartew <andersongularte1993@gmail.com>
2024-04-19 16:28:32 -0300

    java

---
+++ idiomasistema/IdiomaSistema.java
@@ -0,0 +1,8 @@
+package idiomasistema;
+import java.util.*;
+public class IdiomaSistema {
+    public static void main(String[] args) {
+        Locale idioma = new Locale("Portugues","PT");
+        System.out.println("Idioma: " + idioma.getDisplayLanguage());
+    }
+}
---
+++ resolucaotela/ResolucaoTela.java
@@ -0,0 +1,11 @@
+package resolucaotela;
+
+import java.awt.Dimension;
+import java.awt.*;
+public class ResolucaoTela {
+    public static void main(String[] args){
+         Toolkit t = Toolkit.getDefaultToolkit();
+         Dimension d = t.getScreenSize();
+         System.out.println("A resolução da sua máquina é: " +d.width + " x " +d.height);
+    }
+}
