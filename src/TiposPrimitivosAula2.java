5f5e88b4a9bc6f8ed6d54fcfc18fa81bc33d727d
andersongulartew <andersongularte1993@gmail.com>
2024-04-20 12:14:00 -0300

    aulas novas

---
+++ src/TiposPrimitivosAula2.java
@@ -0,0 +1,30 @@
+package src;
+
+
+// * na class * primeira letra de cada palavra sempre maiuscula   , CAMELCASE
+public class TiposPrimitivosAula2 {
+    public static void main(String[] args) {
+//        // variaveis tipos primitivos ( int,double,float, boolean, char, byte, short, long )
+//
+//        int idade = 10; // variavael com letra minuscula;
+//        // A nao ser que tenha duas palavra,entao a partir da segunda se inicia primeira letra maiusculo
+//        System.out.println(" A idade da criança é :" + idade);
+
+
+
+        int idade=10;
+        double salario=1000;
+        float salario2 = 3500;
+        byte idadebyte=10;
+        short idadeshort=110;
+        boolean verdadeiroboolean=true;
+        boolean falsoboolean=false;
+        long numeroGrande=1000L;
+        char caractere = 95;
+        String nome = "william";
+
+        System.out.println(caractere);
+    }
+
+
+}
---
+++ src/TiposPrimitivosExercicio.java
@@ -0,0 +1,14 @@
+package src;
+
+
+// Crie um algoritmo que possua as variaveis : Nome; Endereço e Telefone .
+// Imrpima da seguinte forma: o <nome> domiciliado no endereço <endereço> e telefone <telefone >,
+// não possuem pendencias
+public class TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Anderson";
+        String endereco = "Rua ABC nº 31";
+        String telefone = "51985776172";
+        System.out.println("O " + nome + " domiciliado no endereço : " + endereco + " , e telefone : " + telefone + ", não possuem pendencias");
+    }
+}
