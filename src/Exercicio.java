f042da1ce5d63f01a7d4fd29127f2e1900f18410
andersongulartew <andersongularte1993@gmail.com>
2024-04-22 11:38:18 -0300

    .

--- src/Exercicio.java
+++ src/Exercicio.java
@@ -1,14 +1,40 @@
 package src;
 
 
-// Crie um algoritmo que possua as variaveis : Nome; Endereço e Telefone .
-// Imrpima da seguinte forma: o <nome> domiciliado no endereço <endereço> e telefone <telefone >,
-// não possuem pendencias
 public class Exercicio {
     public static void main(String[] args) {
-        String nome = "Anderson";
-        String endereco = "Rua ABC nº 31";
-        String telefone = "51985776172";
-        System.out.println("O " + nome + " domiciliado no endereço : " + endereco + " , e telefone : " + telefone + ", não possuem pendencias");
+
+// Exercicio 1
+// Crie um algoritmo que possua as variaveis : Nome; Endereço e Telefone .
+// Imrpima da seguinte forma: o <nome> domiciliado no endereço <endereço> e telefone <telefone >,
+// não possuem pendencias .
+
+//        String nome = "Anderson";
+//        String endereco = "Rua ABC nº 31";
+//        String telefone = "51985776172";
+//        System.out.println("O " + nome + " domiciliado no endereço : " + endereco + " , e telefone : " + telefone + ", não possuem pendencias");
+
+
+////////////////////////////////
+
+// Exercicio 2
+
+// Crie um algoritmo que possua as variaveis : Nome; salario ,idade, estado civil .
+// imprima   da seguionte forma: o trabalador <nome>, do sexo <sexo>, idade <idade>,estado civil <estado> e salario<salario>, encontra-se empegado.
+
+        String nome2 = "Anderson";
+        String sexo = "Masculino";
+        float salario = 1500;
+        int idade = 30;
+        String estadoCivil = "solteiro";
+        System.out.println("O trabalhador " + nome2 + ", do sexo :" + sexo + ", idade :" + idade + ", estado civil :" + estadoCivil + " e salario de  :" + salario + ", encontra-se empregado neste local.");
+
+
+
     }
 }
+
+
+
+
+
---
+++ src/Operadores.java
@@ -0,0 +1,26 @@
+package src;
+
+public class Operadores {
+    public static void main(String[] args) {
+        int numero1 = 30;
+        int numero2 = 20;
+
+        int soma = numero1 + numero2;
+        System.out.println(soma);
+        System.out.println("a soma é " + (numero1 + numero2));  // outro metodo de soma sem criar uma variaval
+
+        int multiplicacao = numero1 * numero2;
+        System.out.println(multiplicacao);
+        System.out.println("a multiplicação é " + (numero1 * numero2));
+
+
+        int div = numero1 / numero2;
+        System.out.println(div);
+        System.out.println("a divisão é " + (numero1 / numero2));
+
+        int sub = numero1 - numero2;
+        System.out.println(sub);
+        System.out.println("a subtração é " + (numero1 - numero2));
+
+    }
+}
